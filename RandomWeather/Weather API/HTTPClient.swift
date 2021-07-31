436b06fcbb49af2752d1903c7f5a9526033b405a
Said Rehouni <said_rehouni@hotmail.com>
2021-07-31 12:46:56 +0200

    Moved HTTPClient to tos own file

---
+++ RandomWeather/Weather API/HTTPClient.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPClient.swift
+//  RandomWeather
+//
+//  Created by Said Rehouni on 31/7/21.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
