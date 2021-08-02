9bcbcf42df715ac46c513747e7a7c9627b4647f8
Said Rehouni <said_rehouni@hotmail.com>
2021-08-02 21:54:04 +0200

    Added composition class

---
+++ RandomWeather/Application/Components.swift
@@ -0,0 +1,27 @@
+//
+//  Components.swift
+//  RandomWeather
+//
+//  Created by Said Rehouni on 2/8/21.
+//
+
+import Foundation
+import UIKit
+
+class Components {
+    static func createWeatherComponent() -> UIViewController {
+        let viewModel = WeatherDetailViewModel(weatherLoader: createRemoteweatherLoader(),
+                                               weatherLoaderErrorHandling: RemoteWeatherLoaderErrorHandling())
+        let viewController: WeatherDetailViewController = WeatherDetailViewController.create(viewModel: viewModel)
+        
+        return viewController
+    }
+}
+
+extension Components {
+    static func createRemoteweatherLoader() -> WeatherLoader {
+        RemoteWeatherLoader(urlGenerator: RandomURLGenerator(apiKey: RandomWeatherConstants.apiKey),
+                            client: URLSessionHTTPClient(session: .shared),
+                            mapper: RemoteWeatherLoaderMapper())
+    }
+}
